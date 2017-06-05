5566d399bf43cf5efb4942710823d35702de78ed
Fraser <frasers@hotmail.co.uk>
2017-06-05 12:49:30 +0000

    Created a function that takes a string as a parameter and displays it to the console

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,12 +2,18 @@
   * Created by Fraser on 05/06/2017.
   */
 object Main {
-  def main(args: Array[String]) = {
+
+  //displays a string to the screen
+  def displayStringToConsole(message: String) : Unit ={
+    //display the message
+    println(message)
+  }
+
+  def main(args: Array[String]) : Unit = {
 
     //defines a string
     val message : String = "Hello World"
 
-    //display the message
-    println(message)
+    displayStringToConsole(message)
   }
 }
